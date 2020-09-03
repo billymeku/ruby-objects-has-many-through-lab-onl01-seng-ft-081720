3e4fbb1d0c5102682e95a023f1eb27da56c2e16a
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-03 04:12:16 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,17 +1,3 @@
-# class Appointment
-#   attr_accessor :doctor , :patient , :date
-#   @@all = []
-#   def initialize(date , patient , doctor)
-#     @doctor = doctor
-#     @patient = patient
-#     @date = date
-#     @@all << self
-#   end
-#   def self.all
-#     @@all
-#   end
-# end
-
 class Appointment
 
   attr_accessor :date, :patient, :doctor
